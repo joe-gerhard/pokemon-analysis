55a8030101c7277076dafa56fb0b1ad70dfb9849
Joe Gerhard <jlgerhard@gmail.com>
2020-01-27 10:37:46 -0600

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyledApp } from './styles';
+
+function App() {
+  return (
+    <StyledApp>
+      This is the app
+    </StyledApp>
+  );
+}
+
+export default App;
