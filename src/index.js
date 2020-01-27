0855ca4feb92ce706c2657c0165476b2c38e53f6
Joe Gerhard <jlgerhard@gmail.com>
2020-01-27 10:54:05 -0600

    Setup styled-components and initial folder structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './components/App'
+import theme from './styles/theme';
+import { GlobalStyle } from './styles/GlobalStyle';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
+, document.getElementById('root'));
 
---
+++ src/styles/theme.js
@@ -0,0 +1,6 @@
+export default {
+  primary: '#666666',
+  accent: '#FF0000',
+  dark: '#000000',
+  light: '#AAAAAA'
+}
